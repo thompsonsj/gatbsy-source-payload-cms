942be1aae78d58903515e340841009cac03adaed
Steven Thompson <steven@thompsonsj.com>
2023-06-06 17:42:28 +0100

    minor(*): remove payload response modification

--- plugin/src/format-entity.ts
+++ plugin/src/format-entity.ts
@@ -1,5 +1,4 @@
-import { payloadFieldType } from "./payload-field-type"
-import { isFunction, isPlainObject } from "lodash"
+import { isFunction } from "lodash"
 
 interface IFormatEntry {
   data: { [key: string]: unknown }
@@ -7,66 +6,9 @@ interface IFormatEntry {
   gatsbyNodeType: string
 }
 
-export const parsePayloadResponse = (props: { [key: string]: any }) => {
-  const parsedProps: { [key: string]: any } = {}
-  Object.keys(props).forEach((value) => {
-    /**
-     * Serilaize Slate JSON
-     *
-     * There is no schema to interpret - it is not useful to
-     * query this object in GraphQL. Serialize it to a string
-     * so that it can be converted to JSON when needed in
-     * applications.
-     */
-    if (payloadFieldType(props[value]) === `richText`) {
-      parsedProps[value] = JSON.stringify(props[value])
-    }
-
-    /**
-     * Serilaize images
-     *
-     * Although Payload CMS images have a common schema to
-     * some extent, the sizes array will have different keys
-     * depending on the different image transformations applied.
-     *
-     * TODO create asset nodes and investigate Gatsby's CDN feature
-     */
-    if (payloadFieldType(props[value]) === `upload`) {
-      parsedProps[value] = JSON.stringify(props[value])
-    }
-
-    /**
-     * Serilaize block layouts
-     *
-     * There is no schema to interpret - it is not useful to
-     * query this object in GraphQL. Serialize it to a string
-     * so that it can be converted to JSON when needed in
-     * applications.
-     */
-    if (payloadFieldType(props[value]) === `blocks`) {
-      parsedProps[value] = JSON.stringify(props[value])
-    }
-
-    // support array
-    if (payloadFieldType(props[value]) === `array`) {
-      parsedProps[value] = props[value].map((block: any) => parsePayloadResponse(block))
-    }
-
-    if (payloadFieldType(props[value]) === `other`) {
-      // support groups
-      if (isPlainObject(props[value])) {
-        parsedProps[value] = parsePayloadResponse(props[value])
-      } else {
-        parsedProps[value] = props[value]
-      }
-    }
-  })
-  return parsedProps
-}
-
 export const formatEntity = ({ data, locale, gatsbyNodeType }: IFormatEntry, context?: any) => {
   const res = {
-    ...parsePayloadResponse(data),
+    ...data,
     gatsbyNodeType,
     ...(locale && { locale: locale }),
   }
