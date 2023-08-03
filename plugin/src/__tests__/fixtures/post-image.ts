388a0103cff5f00bdb62a02e58c89e3e5f0d626e
Steven Thompson <steven@thompsonsj.com>
2023-08-03 10:55:22 +0100

    style(post-image): remove unused fixture

--- plugin/src/__tests__/fixtures/post-image.ts
+++
@@ -1,6 +0,0 @@
-export const postImageFixture = {
-  url: `https://images.unsplash.com/photo-1615751072497-5f5169febe17?fm=jpg`,
-  alt: `brown and white long coated dog`,
-  width: 3024,
-  height: 4032,
-}
